27eee4a873f0ced72bbcc84d2ab072a8c68e624d
Qiankun Miao <qiankun.miao@intel.com>
2014-05-08 13:22:36 +0800

    Hello World program
    Basic knowledges of python

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+print 'Hello, World!'
+
+#''' can use to present a multiple line string, or a tring contains ' or "
+print '''He says:
+"It's a dog"'''
+
+# r or R is used to ignore '\'
+print r"Newlines are indicated by \n."
+
+print "Hello," " World!"
+
+# http://forum.ubuntu.org.cn/viewtopic.php?f=63&t=249776
+# The result is -6.
+print ~5
+
+print 'Pi is',  3.14, '.'
